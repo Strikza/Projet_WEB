25862d79c6e6a98ce3e8d1d744e9f511dcd01522
Goubeau Samuel <sams86240@gmail.com>
2021-04-14 11:01:40 +0200

    Add UtilityController

---
+++ code/src/Controller/UtilityController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UtilityController extends AbstractController
+{
+    /**
+     * @Route("/utility", name="utility")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UtilityController.php',
+        ]);
+    }
+}
